ac962a425a2ce4e308a92665f519250ddf920ca0
colin <163393924+officialcc@users.noreply.github.com>
2026-02-23 00:36:00 +0800

    Completed Lesson #623

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import NewPost from "./NewPost";
+import Post from "./Post";
+import Modal from "./Modal";
+import classes from './PostsList.module.css';
+
+function PostsList({ isPosting, onStopPosting }) {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    // let modalContent;
+
+    // if (modalIsVisible) {
+    //     modalContent = (
+    //         <Modal onClose={hideModalHandler}>
+    //             <NewPost
+    //                 onBodyChange={bodyChangeHandler}
+    //                 onAuthorChange={authorChangeHandler}
+    //             />
+    //         </Modal>
+    //     );
+    // }
+
+    return (
+        <>
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
+                <NewPost
+                    onBodyChange={bodyChangeHandler}
+                    onAuthorChange={authorChangeHandler}
+                />
+                </Modal>
+            )}
+            <ul className={classes.posts}>
+                <Post author={enteredAuthor} body={enteredBody} />
+                <Post author="Chin" body="React is silly" />
+            </ul>
+        </>
+    )
+}
+
+export default PostsList;
