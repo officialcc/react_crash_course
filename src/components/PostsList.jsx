1ba490c459e92f3920e644ee8853afce18d8fa22
colin <163393924+officialcc@users.noreply.github.com>
2026-02-24 00:49:18 +0800

    Completed Lesson #636

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import NewPost from "./NewPost";
 import Post from "./Post";
-import Modal from "./Modal";
 import classes from './PostsList.module.css';
 
-function PostsList({ isPosting, onStopPosting }) {
+function PostsList() {
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            setIsFetching(true);
+            const response = await fetch('http://localhost:8080/posts');
+            const resData = await response.json();
+            setPosts(resData.posts);
+            setIsFetching(false);
+        }
+
+        fetchPosts();
+    }, []);
 
     function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         setPosts((existingPosts) => [postData, ...existingPosts]);
     }
 
@@ -27,28 +45,19 @@ function PostsList({ isPosting, onStopPosting }) {
 
     return (
         <>
-            {isPosting && (
-                <Modal onClose={onStopPosting}>
-                <NewPost
-                    // onBodyChange={bodyChangeHandler}
-                    // onAuthorChange={authorChangeHandler}
-                    onCancel={onStopPosting}
-                    onAddPost={addPostHandler}
-                />
-                </Modal>
-            )}
-            {posts.length > 0 && (
+            {!isFetching && posts.length > 0 && (
                 <ul className={classes.posts}>
                     {/* <Post author={enteredAuthor} body={enteredBody} /> */}
                     {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
                 </ul>
             )}
-            {posts.length === 0 && (
+            {!isFetching && posts.length === 0 && (
                 <div style={{textAlign: 'center', color: 'white'}}>
                     <h2>No posts yet.</h2>
                     <p>Start adding some!</p>
                 </div>
             )}
+            {isFetching && <div style={{textAlign: 'center', color: 'white'}}><p>Loading posts ...</p></div>}
         </>
     )
 }
