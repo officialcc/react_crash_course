8b4ca1d67f0c05e2ad06b9cea4de75cc2f773ac6
colin <163393924+officialcc@users.noreply.github.com>
2026-02-23 13:47:55 +0800

    Completed Lesson #625

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,16 +6,6 @@ import Modal from "./Modal";
 import classes from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
 
     // let modalContent;
 
@@ -35,13 +25,14 @@ function PostsList({ isPosting, onStopPosting }) {
             {isPosting && (
                 <Modal onClose={onStopPosting}>
                 <NewPost
-                    onBodyChange={bodyChangeHandler}
-                    onAuthorChange={authorChangeHandler}
+                    // onBodyChange={bodyChangeHandler}
+                    // onAuthorChange={authorChangeHandler}
+                    onCancel={onStopPosting}
                 />
                 </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
+                {/* <Post author={enteredAuthor} body={enteredBody} /> */}
                 <Post author="Chin" body="React is silly" />
             </ul>
         </>
