c4524a3bbcb6156a187f94249bfe8b8b23108542
colin <163393924+officialcc@users.noreply.github.com>
2026-02-25 00:29:01 +0800

    Completed Lesson #639 / React Crash Course

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Posts from './routes/Posts'
-import NewPost from './routes/NewPost'
-import RootLayout from './routes/RootLayout'
-import './index.css'
+import Posts, { loader as postsLoader } from './routes/Posts';
+import NewPost, { action as newPostAction } from './routes/NewPost';
+import PostDetails, { loader as postDetailsLoader} from './routes/PostDetails';
+import RootLayout from './routes/RootLayout';
+import './index.css';
 
 const router = createBrowserRouter([
   {
@@ -15,7 +16,11 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Posts />,
-        children: [{ path: '/create-post', element: <NewPost /> }],
+        loader: postsLoader,
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:postId', element: <PostDetails />, loader: postDetailsLoader }
+        ],
       },
     ],
   },
